9e7b31dbea0e4dc1289ac3ea9f2f2ad437552bce
Aarush Ishan <arushishan0503@gmail.com>
2026-05-01 02:06:41 -0700

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ResponsiveNav from "./components/Home/Navbar/ResponsiveNav";
+import Footer from "./components/Home/Footer/Footer";
+import ScrollToTop from "./components/Helper/ScrollToTop";
+
+const font=Inter({
+  weight:['100','200','300','400','500','600','700','800','900'],
+  subsets:['latin'],
+})
+
+export const metadata: Metadata = {
+  title: "Aarush Portfoliyo | Next.js 15",
+  description: "Portfoliyo built with Next.js 15",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased bg-[#0d0d1f]`}>
+        <ResponsiveNav />
+        {children}
+        <Footer />
+        <ScrollToTop />
+        </body>
+    </html>
+  );
+}
---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Home from './components/Home/Hero/Services/Home';
+"@/components/HomePage/HomePage";
+
+import React from 'react';
+
+const HomePage = () => {
+  return <Home />;
+};
+
+export default HomePage;
